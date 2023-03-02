6bb8dc2e61794765fe982241b5a8e8ef351895f5
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-03-02 16:16:01 -0300

    google

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './app/signinForm.js';
 import './app/signupForm.js';
 import './app/logout.js';
+import './app/googleLogin.js'
 import { loginCheck } from './app/loginCheck.js'
 
 import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
