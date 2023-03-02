6e95e21316a826a3a97eb19c7e56728229330c52
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-03-02 15:40:45 -0300

    Iniciar Sesion Auth

--- src/app/signinForm.js
+++ src/app/signinForm.js
@@ -1,10 +1,21 @@
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { auth } from './firebase.js';
+import { showMessage } from './showMessage.js';
+
 const signInForm = document.querySelector("#login");
 
-signInForm.addEventListener('submit', e => {
+signInForm.addEventListener('submit', async e => {
     e.preventDefault();
 
     const email = signInForm['login-email'].value;
     const password = signInForm['login-password'].value;
 
-    console.log(`El usuario ${email} ha iniciado Sesion donde su contraseña es: ${password}`);
+    try {
+        const credentials = await signInWithEmailAndPassword(auth,email,password);
+        showMessage("Iniciado Correctamente","success");
+    } catch (error) {
+        if(error.code === "auth/network-request-failed"){
+            showMessage("Error al iniciar sesion","error");
+        }
+    }
 })
